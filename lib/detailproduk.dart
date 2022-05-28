2b39bc7396eb09aff94127867bc0336b3f72ec86
FlutLab.io <>
2022-05-28 09:35:43 +0000

    Autosave

--- lib/detailproduk.dart
+++ lib/detailproduk.dart
@@ -133,7 +133,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                       child: Row(
                     children: <Widget>[
                       Text(
-                        " Rp." + widget.promotionPrice,
+                        " Rp." + widget.price,
                         style: TextStyle(fontSize: 16, height: 1.5),
                       ),
                     ],
