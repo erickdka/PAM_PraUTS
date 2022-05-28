e26458dc0a91bb0a6292477aeea011926cce67a7
FlutLab.io <>
2022-05-28 10:12:09 +0000

    Autosave

---
+++ lib/dataproduk.dart
@@ -0,0 +1,188 @@
+List dataItems = [
+  {
+    "id": 1,
+    "code": "001",
+    "name": "Redmi Note 9",
+    "img": "https://i.postimg.cc/w71zR7jR/redmi9.jpg",
+    "price": 2000000,
+    "color": [
+      {
+        "id": 1,
+        "value": "https://i.postimg.cc/w71zR7jR/redmi9.jpg"
+      },
+      {
+        "id": 2,
+        "value": "https://i.postimg.cc/pyGxdDRt/redmi91.jpg"
+      }
+    ],
+    "size": [
+      {
+        "id": 1,
+        "value": "32"
+      },
+      {
+        "id": 2,
+        "value": "64"
+      },
+      {
+        "id": 3,
+        "value": "128"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "code": "002",
+    "name": "Redmi Note 10",
+    "img": "https://i.postimg.cc/sMSRKqSV/redmi10.jpg",
+    "price": 2000000,
+    "color": [
+      {
+        "id": 1,
+        "value": "https://i.postimg.cc/sMSRKqSV/redmi10.jpg",
+      },
+      {
+        "id": 2,
+        "value": "https://i.postimg.cc/JGSLNMs6/redmi101.jpg"
+      }
+    ],
+    "size": [
+      {
+        "id": 1,
+        "value": "32"
+      },
+      {
+        "id": 2,
+        "value": "64"
+      },
+      {
+        "id": 3,
+        "value": "128"
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "code": "003",
+    "name": "Redmi Note 9",
+    "img": "https://i.postimg.cc/w71zR7jR/redmi9.jpg",
+    "price": 2000000,
+    "color": [
+      {
+        "id": 1,
+        "value": "https://i.postimg.cc/w71zR7jR/redmi9.jpg"
+      },
+      {
+        "id": 2,
+        "value": "https://i.postimg.cc/pyGxdDRt/redmi91.jpg"
+      }
+    ],
+    "size": [
+      {
+        "id": 1,
+        "value": "32"
+      },
+      {
+        "id": 2,
+        "value": "64"
+      },
+      {
+        "id": 3,
+        "value": "128"
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "code": "004",
+    "name": "Redmi Note 10",
+    "img": "https://i.postimg.cc/sMSRKqSV/redmi10.jpg",
+    "price": 2000000,
+    "color": [
+      {
+        "id": 1,
+        "value": "https://i.postimg.cc/sMSRKqSV/redmi10.jpg",
+      },
+      {
+        "id": 2,
+        "value": "https://i.postimg.cc/JGSLNMs6/redmi101.jpg"
+      }
+    ],
+    "size": [
+      {
+        "id": 1,
+        "value": "32"
+      },
+      {
+        "id": 2,
+        "value": "64"
+      },
+      {
+        "id": 3,
+        "value": "128"
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "code": "005",
+    "name": "Redmi Note 9",
+    "img": "https://i.postimg.cc/w71zR7jR/redmi9.jpg",
+    "price": 2000000,
+    "color": [
+      {
+        "id": 1,
+        "value": "https://i.postimg.cc/w71zR7jR/redmi9.jpg"
+      },
+      {
+        "id": 2,
+        "value": "https://i.postimg.cc/pyGxdDRt/redmi91.jpg"
+      }
+    ],
+    "size": [
+      {
+        "id": 1,
+        "value": "32"
+      },
+      {
+        "id": 2,
+        "value": "64"
+      },
+      {
+        "id": 3,
+        "value": "128"
+      }
+    ]
+  },
+  {
+    "id": 6,
+    "code": "006",
+    "name": "Redmi Note 10",
+    "img": "https://i.postimg.cc/sMSRKqSV/redmi10.jpg",
+    "price": 2000000,
+    "color": [
+      {
+        "id": 1,
+        "value": "https://i.postimg.cc/sMSRKqSV/redmi10.jpg",
+      },
+      {
+        "id": 2,
+        "value": "https://i.postimg.cc/JGSLNMs6/redmi101.jpg"
+      }
+    ],
+    "size": [
+      {
+        "id": 1,
+        "value": "32"
+      },
+      {
+        "id": 2,
+        "value": "64"
+      },
+      {
+        "id": 3,
+        "value": "128"
+      }
+    ]
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,112 @@
 import 'package:flutter/material.dart';
+import 'dataproduk.dart';
+import 'detailproduk.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  int activeMenu = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 20,
+              right: 20,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Toko Online",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Wrap(
+            children: List.generate(dataItems.length, (index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => ProductDetailPage(
+                                id: dataItems[index]['id'].toString(),
+                                name: dataItems[index]['name'],
+                                code: dataItems[index]['code'],
+                                img: dataItems[index]['img'],
+                                price: dataItems[index]['price'].toString(),
+                                promotionPrice: dataItems[index]['promotionPrice'].toString(),
+                                size: dataItems[index]['size'],
+                                color: dataItems[index]['color'],
+                              )));
+                },
+                child: Card(
+                    elevation: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Hero(
+                          tag: dataItems[index]['id'].toString(),
+                          child: Container(
+                            width: (size.width - 16) / 2,
+                            height: (size.width - 16) / 2,
+                            decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(dataItems[index]['img']), fit: BoxFit.cover)),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text(
+                            dataItems[index]['name'],
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text(
+                            "Rp." + dataItems[index]['price'].toString(),
+                            style: TextStyle(fontSize: 16, color: Colors.red),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    )),
+              );
+            }),
+          )
+        ],
       ),
     );
   }
